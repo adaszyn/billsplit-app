da9b5dc3ec01ca25a3a0d2786d75cdee1e95ac96
Oscar Melin <oscarme@kth.se>
2018-12-04 16:44:20 +0100

    Add navigate to swish qr screen to payments

--- screens/qr-swish.screen.js
+++ screens/qr-swish.screen.js
@@ -1,32 +1,29 @@
 import React from "react";
 import {
   View,
-  Text,
   Image,
-  Button,
-  TouchableOpacity,
   StyleSheet,
 } from "react-native";
 
+import { Colors } from "../config/theme.config";
+
 export class QrSwishScreen extends React.Component {
+  static navigationOptions = {
+    headerStyle: {
+      backgroundColor: Colors.main
+    },
+    headerTintColor: "#fff"
+  };
+
   render() {
-    const { goBack } = this.props.navigation;
+    const payment = this.props.navigation.getParam('payment');
 
     return (
       <View style={styles.container}>
         <Image
-          source={{uri: "https://image-proxy-lamswmfwgf.now.sh/?payee=0730694740&amount=100.0"}}
+          source={{uri: payment.qrUri()}}
           style={{width: 400, height: 400}}
         />
-        <Text style={styles.text}>
-          Use this code to pay in Swish.
-        </Text>
-        <TouchableOpacity style={styles.button}>
-          <Button
-            title="Ok"
-            onPress={() => goBack()}
-          />
-        </TouchableOpacity>
       </View>
     );
   };
@@ -37,26 +34,5 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-  },
-  text: {
-    margin: 30,
-    fontWeight: "bold",
-    fontSize: 30,
-  },
-  button: {
-    width: 150,
   }
 });
-
-const requestBody = {
-  "payee": {
-    "value": "0730694740",
-    "editable": false
-  },
-  "amount": {
-    "value": "10",
-    "editable": false
-  },
-  "format": "jpg",
-  "size": 400
-};
