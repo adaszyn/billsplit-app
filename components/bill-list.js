751ba671e9cf53469e43f2f8a54152e4b28b97f0
Wojciech Adaszyński <wojciech.adaszynski@gmail.com>
2018-12-08 23:54:14 +0100

    No bills placeholder

--- components/bill-list.js
+++ components/bill-list.js
@@ -1,6 +1,5 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
-import { BillListComponent } from "./bill-list-item";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Colors } from "../config/theme.config";
 import {
   Container,
@@ -15,29 +14,34 @@ import {
   ActionSheet
 } from "native-base";
 import { observer } from "mobx-react";
-import { store } from '../stores/main-store';
+import { store } from "../stores/main-store";
 
+const EmptyListPlaceholder = () => (
+  <View style={styles.placeholderContainer}>
+    <Icon name="pricetag" style={styles.placeholderIcon} active />
+    <Text style={styles.placeholderText}>No bills</Text>
+  </View>
+);
 @observer
 export class BillList extends React.Component {
-    // componentDidMount() {
-        // this.props.navigation.navigate("Bill", { bill: this.props.bills[0]})
-    // }
-    onBillSelect = (bill) => {
-        const { navigation } = this.props;
-        store.currentBill = bill;
-        navigation.navigate("Bill");
-    }
+  // componentDidMount() {
+  // this.props.navigation.navigate("Bill", { bill: this.props.bills[0]})
+  // }
+  onBillSelect = bill => {
+    const { navigation } = this.props;
+    store.currentBill = bill;
+    navigation.navigate("Bill");
+  };
   renderListItem = bill => {
     const { navigation, removeBill } = this.props;
     return (
-      <TouchableOpacity
-        key={bill.id}
-      >
+      <TouchableOpacity key={bill.id}>
         <ListItem thumbnail onPress={() => this.onBillSelect(bill)}>
           <Body>
             <Text>{bill.name}</Text>
             <Text note numberOfLines={1}>
-                {bill.participants.length} participants, {bill.numberOfExpenses} items
+              {bill.participants.length} participants, {bill.numberOfExpenses}{" "}
+              items
             </Text>
           </Body>
           <Right>
@@ -53,41 +57,56 @@ export class BillList extends React.Component {
       </TouchableOpacity>
     );
   };
-  render() {
+  renderFab = () => {
     const { bills, navigation } = this.props;
+    return (
+      <Fab
+        active
+        direction="up"
+        containerStyle={{}}
+        style={{ backgroundColor: Colors.main }}
+        position="bottomRight"
+        onPress={() => {
+          const buttons = [
+            { text: "Simple bill", icon: "md-person" },
+            { text: "Shareable bill", icon: "md-people" },
+            { text: "Cancel", icon: "md-close" }
+          ];
+          ActionSheet.show(
+            {
+              options: buttons,
+              cancelButtonIndex: buttons.length - 1,
+              title: "Create bill"
+            },
+            buttonIndex => {
+              if (buttonIndex === 0) {
+                navigation.navigate("CreateBill", { type: "simple" });
+              } else if (buttonIndex === 1) {
+                console.warn("Not implemented yet");
+              }
+            }
+          );
+        }}
+      >
+        <Icon name="add" />
+      </Fab>
+    );
+  };
+  render() {
+    const { bills } = this.props;
+    const isEmpty = bills.length === 0;
+    if (isEmpty) {
+      return (
+        <>
+          <EmptyListPlaceholder />
+          {this.renderFab()}
+        </>
+      );
+    }
     return (
       <Container>
         <List>{bills.map(this.renderListItem)}</List>
-        <Fab
-          active
-          direction="up"
-          containerStyle={{}}
-          style={{ backgroundColor: Colors.main }}
-          position="bottomRight"
-          onPress={() => {
-            const buttons = [
-              { text: "Simple bill", icon: "md-person" },
-              { text: "Shareable bill", icon: "md-people" },
-              { text: "Cancel", icon: "md-close"}
-            ];
-            ActionSheet.show(
-              {
-                options: buttons,
-                cancelButtonIndex: buttons.length - 1,
-                title: "Create bill"
-              },
-              buttonIndex => {
-                if (buttonIndex === 0) {
-                  navigation.navigate("CreateBill", { type: "simple" });
-                } else if (buttonIndex === 1) {
-                  console.warn("Not implemented yet")
-                }
-              }
-            )
-          }}
-        >
-          <Icon name="add" />
-        </Fab>
+        {this.renderFab()}
       </Container>
     );
   }
@@ -95,5 +114,23 @@ export class BillList extends React.Component {
 const styles = StyleSheet.create({
   container: {
     borderRadius: 4
+  },
+  placeholderText: {
+    color: Colors.grey,
+    fontSize: 20,
+    textAlign: "center",
+    marginTop: 20
+  },
+  placeholderContainer: {
+    alignSelf: "stretch",
+    flex: 1,
+    justifyContent: "center",
+    alignContent: "center"
+  },
+  placeholderIcon: {
+    fontSize: 80,
+    color: Colors.grey,
+    padding: 0,
+    textAlign: "center"
   }
 });
